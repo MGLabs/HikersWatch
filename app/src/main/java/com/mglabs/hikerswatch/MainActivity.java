42d5c47b3c289e423ad8d854dd75b14fb7d74a5f
MGLabs <goldwings3@hotmail.com>
2018-05-02 20:35:05 +0200

    Address added

--- app/src/main/java/com/mglabs/hikerswatch/MainActivity.java
+++ app/src/main/java/com/mglabs/hikerswatch/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -16,12 +18,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     LocationManager locationManager;
     LocationListener locationListener;
 
-    @Override
+    @Override //questo è il metodo che gestisce lo yes/no del dialog che richiede i permessi
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -82,14 +88,14 @@ public class MainActivity extends AppCompatActivity {
             //if permission is not granted
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-                //ask permission
+                //ask permission  (1 è la request identifier for the other end. In questo caso abbiamo una richiesta sola quindi non è importante.
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
             } else {
 
                 //if permission already granted
                 locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 0, locationListener);
                 Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
                 if (location != null) {
@@ -103,14 +109,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void updateLocationInfo(Location location) {
-        Log.i("LocationInfo", location.toString());
+        //Log.i("LocationInfo", location.toString());
 
         TextView latTextView = findViewById(R.id.latTextView);
         TextView lonTextView = findViewById(R.id.lonTextView);
         TextView altTextView = findViewById(R.id.altTextView);
-        TextView addTextView = findViewById(R.id.addTextView);
+        TextView accTextView = findViewById(R.id.accTextView);
 
-        latTextView.setText("Latitude:" + location.getLatitude());
-        lonTextView.setText("Longitude:" + location.getLongitude());
+        latTextView.setText("Latitude: " + location.getLatitude());
+        lonTextView.setText("Longitude: " + location.getLongitude());
+        altTextView.setText("Altitude: " + location.getAltitude());
+        accTextView.setText("Address: " + location.getAccuracy());
+
+        //Geocoder object for the address
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        try {
+            List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+Log.i("LISTA DI ADDRESS", addressList.toString());
+
+            if (addressList != null && addressList.size() > 0) {
+
+                Log.i("PlaceInfo", addressList.get(0).toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
